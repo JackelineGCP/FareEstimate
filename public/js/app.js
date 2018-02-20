9c3c9e06278aa39c0c8a4dc8925a843306cb71e3
JackelineGCP <gessenia_17@hotmail.com>
2018-02-20 12:00:49 -0500

    agregando configuracion de firbase

---
+++ public/js/app.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  var config = {
+    apiKey: 'AIzaSyAVSRjY-r0Bg97pF96wQyHtkWBg3f9uhbA',
+    authDomain: 'fareestimate.firebaseapp.com',
+    databaseURL: 'https://fareestimate.firebaseio.com',
+    projectId: 'fareestimate',
+    storageBucket: 'fareestimate.appspot.com',
+    messagingSenderId: '614888306128'
+  };
+  firebase.initializeApp(config); 
+});
